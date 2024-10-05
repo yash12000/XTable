37940452a33be9367fe76f00170ed496f21803e1
yash12000 <154689285+yash12000@users.noreply.github.com>
2024-10-05 17:54:22 +0530

    Initial commit

---
+++ src/XTable.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+
+const XTable = () => {
+  const [data, setData] = useState([
+    { date: "2022-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-01", views: 100, article: "Article 1" },
+    { date: "2023-09-02", views: 150, article: "Article 2" },
+    { date: "2023-09-02", views: 120, article: "Article 3" },
+    { date: "2020-09-03", views: 200, article: "Article 4" },
+  ]);
+
+  const sortByDate = () => {
+    const sortedData = [...data].sort((a, b) => {
+      if (a.date === b.date) {
+        return b.views - a.views;
+      }
+      return new Date(b.date) - new Date(a.date);
+    });
+    setData(sortedData);
+  };
+
+  const sortByViews = () => {
+    const sortedData = [...data].sort((a, b) => {
+      if (a.views === b.views) {
+        return new Date(b.date) - new Date(a.date);
+      }
+      return b.views - a.views;
+    });
+    setData(sortedData);
+  };
+
+  return (
+    <div>
+      <h1>Date and Views Table</h1>
+      <button onClick={sortByDate}>Sort by Date</button>
+      <button onClick={sortByViews}>Sort by Views</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Views</th>
+            <th>Article</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.date}</td>
+              <td>{item.views}</td>
+              <td>{item.article}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default XTable;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import XTable from "./XTable";
+
+const App = () => {
+  return (
+    <div>
+      <XTable />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
